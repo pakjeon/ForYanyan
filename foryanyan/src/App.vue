f59889b870b875d680d208190f9a35afc0357eef
麦柏润 <maibairun@163.com>
2019-04-16 23:16:13 +0800

    init

---
+++ foryanyan/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+      <clock></clock>
+  </div>
+</template>
+
+<script>
+import clock from './components/clock.vue'
+
+export default {
+  name: 'app',
+  components: {
+    clock
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ foryanyan/src/components/clock.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="clock">
+        this is a {{ clock }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'clock',
+        props: {
+        },
+        data() {
+            return {
+                clock: 'clock',
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
