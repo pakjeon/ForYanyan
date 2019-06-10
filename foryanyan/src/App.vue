cf2cb144d077c3320560b140ab30c9349881e16d
麦柏润 <maibairun@163.com>
2019-06-10 20:51:30 +0800

    搭建完成

--- foryanyan/src/App.vue
+++ foryanyan/src/App.vue
@@ -1,22 +1,81 @@
 <template>
   <div id="app">
-      <clock :d="day" :h="hour" :m="minute" :s="second"></clock>
-      <div class="coverflow-container">
-          <coverflow :coverList="coverList" :coverWidth="360" :coverHeight="240" :index="coverIndex" :coverSpace="100" :coverShadow="false"></coverflow>
+      <clock :d="day" :h="hour" :m="minute" :s="second" class="clock"></clock>
+<!--      <line-star></line-star>-->
+<!--      <div class="coverflow-container">-->
+<!--          <coverflow :coverList="coverList" :coverWidth="360" :coverHeight="240" :index="coverIndex" :coverSpace="100" :coverShadow="false"></coverflow>-->
+<!--      </div>-->
+      <div class="main">
+<!--          <audio src="../src/assets/music/a.mp3" autoplay="autoplay"></audio>-->
+          <!-- swiper1 -->
+          <swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop">
+              <swiper-slide class="slide-1"></swiper-slide>
+              <swiper-slide class="slide-2"></swiper-slide>
+              <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+              <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+          </swiper>
+          <!-- swiper2 Thumbs -->
+          <swiper :options="swiperOptionThumbs" class="gallery-thumbs" ref="swiperThumbs">
+              <swiper-slide class="slide-1"></swiper-slide>
+              <swiper-slide class="slide-2"></swiper-slide>
+          </swiper>
       </div>
+
   </div>
 </template>
 
 <script>
 import clock from './components/clock.vue'
-import coverflow from 'vue-coverflow';
-import Fireworks from '../src/common/utils'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+// import lineStar from './components/lineStar.vue'
+// import coverflow from 'vue-coverflow';
+// import Fireworks from '../src/common/utils'
 
 export default {
   name: 'app',
   components: {
     clock,
-    coverflow,
+    // lineStar,
+    // coverflow,
+    swiper,
+    swiperSlide,
+  },
+  data() {
+    return {
+      day: 0,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      swiperOptionTop: {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      },
+      swiperOptionThumbs: {
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        touchRatio: 0.2,
+        slideToClickedSlide: true
+      },
+      coverIndex: 0,
+      coverList: [
+        {
+          cover: require('../src/assets/photo/1 (1).png'),
+          title: '猪猪燕',
+        },
+        {
+          cover: require('../src/assets/photo/1 (2).png'),
+          title: '燕仔',
+        },
+        {
+          cover: require('../src/assets/photo/1 (3).png'),
+          title: '燕儿',
+        },
+      ],
+    };
   },
   created() {
     // for (let i = 0; i < 6; i++) {
@@ -27,15 +86,23 @@ export default {
     //   });
     // }
     setInterval(this.timer, 1000);
-
-    document.onclick = function (e){
-      let evt=e||window.event;	//兼容性处理
-      for(let i=0;i<20;i++){		//随机烟花的数量
-        let div=document.createElement("div");
-        let b=new Fireworks(div,evt.pageX,evt.pageY);
-        b.move();
-      }
-    }
+    //
+    // document.onclick = function (e){
+    //   let evt=e||window.event;	//兼容性处理
+    //   for(let i=0;i<20;i++){		//随机烟花的数量
+    //     let div=document.createElement("div");
+    //     let b=new Fireworks(div,evt.pageX,evt.pageY);
+    //     b.move();
+    //   }
+    // }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      const swiperTop = this.$refs.swiperTop.swiper;
+      const swiperThumbs = this.$refs.swiperThumbs.swiper;
+      swiperTop.controller.control = swiperThumbs;
+      swiperThumbs.controller.control = swiperTop;
+    });
   },
   methods: {
     timer() {
@@ -59,41 +126,32 @@ export default {
       this.minute = m;
       this.second = s;
     },
-},
-  data() {
-    return {
-      day: 0,
-      hour: 0,
-      minute: 0,
-      second: 0,
-      coverIndex: 0,
-      coverList: [
-        {
-          cover: require('../src/assets/photo/1 (1).png'),
-          title: '猪猪燕',
-        },
-        {
-          cover: require('../src/assets/photo/1 (2).png'),
-          title: '燕仔',
-        },
-        {
-          cover: require('../src/assets/photo/1 (3).png'),
-          title: '燕儿',
-        },
-      ],
-    };
   },
 }
 </script>
 
 
 <style lang="scss">
+    body {
+        margin: 0;
+    }
 #app {
-    background-color: #ccc;
+    background: -webkit-linear-gradient(aqua, pink); /* Safari 5.1 - 6.0 */
+    background: -o-linear-gradient(aqua, pink); /* Opera 11.1 - 12.0 */
+    background: -moz-linear-gradient(aqua, pink); /* Firefox 3.6 - 15 */
+    background: linear-gradient(aqua, pink); /* 标准的语法 */
+    height: 100vh;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+    /*display: flex;*/
+    /*align-items: center;*/
+    /*flex-direction: column;*/
+    /*justify-content: space-between;*/
 }
+    .clock {
+        width: 100%;
+    }
     .coverflow-container {
         width: 980px;
         margin: 0 auto;
@@ -104,5 +162,111 @@ export default {
         text-shadow: 1px 1px 1px #ccc;
         /*border-radius: 50%;*/
         position: absolute;
+    }
+    .main {
+        width: 1080px;
+        height: 500px;
+        background-color: #f2f2f2;
+        margin: 0 auto;
+        .swiper-container {
+            background: #fefefe;
+            margin: 0 auto;
+            position: relative;
+            overflow: hidden;
+            list-style: none;
+            padding: 0;
+            z-index: 1;
+        }
+        .swiper-wrapper {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            transition-property: -webkit-transform;
+            transition-property: transform;
+            transition-property: transform,-webkit-transform;
+            box-sizing: content-box;
+        }
+        .swiper-slide {
+            text-align: center;
+            font-size: 38px;
+            font-weight: 700;
+            background-color: #eee;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+            flex-shrink: 0;
+            width: 100%;
+            height: 100%;
+            position: relative;
+            transition-property: -webkit-transform;
+            transition-property: transform;
+            transition-property: transform,-webkit-transform;
+            &.slide-1 {
+                background: url('../src/assets/photo/IMG_6096.png') center no-repeat;
+                background-size: 100%;
+            }
+            &.slide-2 {
+                background: url('../src/assets/photo/IMG_6096.png') center no-repeat;
+                background-size: 100%;
+            }
+        }
+        .swiper-button-next,.swiper-button-prev {
+            position: absolute;
+            top: 50%;
+            width: 27px;
+            height: 44px;
+            margin-top: -22px;
+            z-index: 10;
+            cursor: pointer;
+            background-size: 27px 44px;
+            background-position: 50%;
+            background-repeat: no-repeat;
+        }
+        .swiper-button-prev {
+            /*background-color: red;*/
+            background: url("../src/assets/photo/left.png") 50% 50% no-repeat;
+            background-size: cover;
+            left: 10px;
+            right: auto;
+        }
+        .swiper-button-next {
+            /*background-color: green;*/
+            background: url("../src/assets/photo/right.png") 50% 50% no-repeat;
+            background-size: cover;
+            right: 10px;
+            left: auto;
+        }
+        .swiper-button-disabled {
+            opacity: .35;
+            cursor: auto;
+            pointer-events: none;
+        }
+        .gallery-top {
+            height: 80%!important;
+            width: 100%;
+        }
+        .gallery-thumbs {
+            height: 20%!important;
+            box-sizing: border-box;
+            padding: 10px 0;
+        }
+        .gallery-thumbs .swiper-slide {
+            width: 25%;
+            height: 100%;
+            opacity: 0.4;
+        }
+        .gallery-thumbs .swiper-slide-active {
+            opacity: 1;
+        }
     }
 </style>
--- foryanyan/src/components/clock.vue
+++ foryanyan/src/components/clock.vue
@@ -30,5 +30,13 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+    .clock {
+        font-size: 50px;
+        /*background-color: aqua;*/
+        color: pink;
+        margin: 0 auto;
+        padding: 50px 0;
+        text-align: center;
 
+    }
 </style>
